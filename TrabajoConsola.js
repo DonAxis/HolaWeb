e62433688db3a9a7f69b169e6b1062fedce8993f
Alexis G <53626202+DonAxis@users.noreply.github.com>
2025-10-21 21:58:55 -0600

    Add files via upload
    nada

---
+++ TrabajoConsola.js
@@ -0,0 +1,42 @@
+var datos=["casa","lapiz","estudiante","color"];
+
+function prueba1(){
+    console.log ("datos: " +datos);
+}
+
+prueba1();
+
+datos.forEach(function(elemento){
+    console.log (datos);
+});
+
+datos.forEach(function(elemento){
+    console.log (elemento);
+});
+
+var coche={
+    marca: "ford",
+    motor: "electrioc",
+    acelerar: function(){
+        console.log("amonos");
+    }
+};
+
+coche.acelerar();
+console.log(coche.marca);
+
+//dom es el documento HTML y se usa con document
+var elDom = document.querySelector("h1");
+elDom.textContent ="ahora ya cambia";
+
+//evento susesos como dar click cualquier cosa
+// listener escucha o recibe los datos
+
+function Colorea(){
+    this.classList.toggle("cambia");
+    console.log("ajam");
+    elDom.textContent ="uwu";
+
+}
+document.querySelector("body").addEventListener("click", Colorea);
+
