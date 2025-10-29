5164355f82c993e705edf6c658d2c2dae7b71c32
Alexis Gonzalez <alexis-axis@hotmail.com>
2025-10-29 02:37:37 -0600

    agrego la pagina completa

--- arreglos.js
+++ arreglos.js
@@ -100,3 +100,39 @@ console.warn("SPLIT, convierte el String a Array la frace: '",frase,"' ,asi qued
 const columnas = csv.split(",");
 const partes = email.split("@");
 console.error("podemos usar ',' '@' u otro separador, tengo: ",csv,"y",email,"\n asi queda:", columnas, " y ", partes);
+
+// puedes combinar varios métodos y obtener resultados complejos
+const productos = [
+    { nombre: "compu", precio: 15000, categoria: "tecnologia" },
+    { nombre: "Mouse", precio: 300, categoria: "tecnologia" },
+    { nombre: "SillaGamer", precio: 2500, categoria: "muebles" },
+    { nombre: "Teclado", precio: 800, categoria: "tecnologia" },
+    { nombre: "posterPared", precio: 5000, categoria: "muebles" }
+];
+
+
+/* filtraremos primero por "tecnologia" (filter),
+ ordenaremos por precio de menor a mayor (sort),
+ tomamos nombres (map),
+ y 
+ unimos nombres en un solo string separado por '>' (join) */
+
+ //const resultado = productos.filter(p => p.categoria === "tecnologia").sort((a, b) => a.precio - b.precio).map(p => p.nombre).join(" o ");
+//puedes escribirlo en varios renglones
+ const resultado = productos
+    .filter(p => p.categoria === "tecnologia")
+    .sort((a, b) => a.precio - b.precio)
+    .map(p => p.nombre)
+	.join(" > ");
+console.warn("Productos de tecnología ordenados por precio:", resultado);
+
+// sumaremos el precio de tecnoloia
+/* 
+volvemos a filtrar "tecnologia" (filter),
+ y luego sumamos (reduce) */
+
+const aPagar = productos
+    .filter(X => X.categoria === "tecnologia")
+    .reduce((total, X) => total + X.precio, 0);
+
+console.warn("Total de productos de tecnología: $" + aPagar);
