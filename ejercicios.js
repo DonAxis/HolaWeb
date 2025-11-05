cb9f10f3aa3ae368b7eab870686a91af6dd97b42
Alexis Gonzalez <alexis-axis@hotmail.com>
2025-11-05 02:00:16 -0600

    ultimo ajuste subido

--- ejercicios.js
+++ ejercicios.js
@@ -1,1 +1,21 @@
+<<<<<<< HEAD
 
+=======
+const productos = [
+  { nombre: "compu", precio: 15000, categoria: "tecnologia" },
+  { nombre: "Mouse", precio: 300, categoria: "tecnologia" },
+  { nombre: "Teclado", precio: 800, categoria: "tecnologia" },
+  { nombre: "Monitor", precio: 4500, categoria: "tecnologia" },
+  { nombre: "Auriculares", precio: 1200, categoria: "tecnologia" },
+  { nombre: "Router", precio: 2000, categoria: "tecnologia" },
+  { nombre: "telefono", precio: 8000, categoria: "tecnologia" },
+  { nombre: "SillaGamer", precio: 3500, categoria: "muebles" },
+  { nombre: "tenis", precio: 2000, categoria: "ropa" },
+  { nombre: "Escritorio", precio: 4000, categoria: "muebles" },
+  { nombre: "blusa", precio: 500, categoria: "ropa" },
+  { nombre: "Cafetera", precio: 1800, categoria: "cafe" },
+  { nombre: "CamisaGodin", precio: 100, categoria: "ropa" },
+  { nombre: "Pantalon", precio: 1200, categoria: "ropa" },
+  { nombre: "taza", precio: 300, categoria: "cafe" }
+];
+>>>>>>> a4b2a19 (ajustes)
