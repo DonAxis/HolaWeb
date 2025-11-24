5d2e53d291baff8cfa333cac9f6e5cd0e2589f46
DonAxis <alexis-axis@hotmail.com>
2025-11-24 02:34:52 -0600

    casita

--- art.js
+++ art.js
@@ -1,3 +1,37 @@
+  const canvas = document.createElement('canvas');
+canvas.width=500;
+canvas.height=500;
+document.body.appendChild(canvas);
+const ctx = canvas.getContext('2d');
+
+ctx.lineWidth = 8; // ancho linea
+
+ctx.fillStyle='yellow';
+ctx.fillRect(230, 160, 50, 50); //rectangulo luz
+
+ctx.strokeRect(230, 160, 50, 50); //rectangulo ventana
+ctx.strokeRect(230, 160, 25, 25); //rectangulo ventana marco
+ctx.strokeRect(230, 185, 25, 25); //rectangulo ventana marco
+ctx.strokeRect(255, 160, 25, 25); //rectangulo ventana marco
+ctx.fillStyle='brown';
+ctx.strokeRect(130, 190, 40, 60); //rectangulo puerta
+ctx.fillRect(130, 190, 40, 60); //rectangulo puerta
+ctx.strokeRect(75, 140, 250, 110); //rectangulo casa
+
+ctx.beginPath(); //poligono techo
+ctx.moveTo(50, 140);
+ctx.lineTo(200, 60);
+ctx.lineTo(350, 140);
+ctx.closePath();
+ctx.stroke();  //fin poligono
+
+ctx.fillStyle='red';
+
+ctx.font = "30px serif"; //texto
+ctx.fillText("Casita", 90, 170)
+
+
+
 /*const canvasSketch = require('canvas-sketch');
 
 const settings = {
@@ -14,21 +48,15 @@ const sketch = () => {
 canvasSketch(sketch, settings);
 */
 
-  const canvas = document.createElement('canvas');
-canvas.width=300;
-canvas.height=300;
-document.body.appendChild(canvas);
-const ctx = canvas.getContext('2d');
 
-ctx.lineWidth = 8; // ancho linea
 
-ctx.strokeRect(75, 140, 150, 110); //rectangulo
 
-ctx.fillRect(130, 190, 40, 60); //rectangulo con relleno
 
-ctx.beginPath(); //poligono
-ctx.moveTo(50, 140);
-ctx.lineTo(150, 60);
-ctx.lineTo(250, 140);
-ctx.closePath();
-ctx.stroke();
+
+
+
+
+
+
+
+
