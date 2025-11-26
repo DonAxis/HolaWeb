85b14481f7b1c836e5586af0f389379cdc178d84
DonAxis <alexis-axis@hotmail.com>
2025-11-26 02:48:03 -0600

    canvas-sketch incorporado

--- art.js
+++ art.js
@@ -1,66 +1,58 @@
-function borracasita(){
-  const primera = document.getElementById("miCanvas");
-  if (primera) primera.remove();
+/*
+npm install canvas-sketch-cli -g
+canvas-sketch art.js --open
+canvas-sketch art.js --new --open
+*/
+// info https://github.com/mattdesl/canvas-sketch
 
-}
-XX=0;
-function  casita(){
-  
-  const canvas = document.createElement('canvas');
-   canvas.remove();
-  canvas.width=400;
-  canvas.height=300;
-  document.body.appendChild(canvas);
-  const ctx = canvas.getContext('2d');
-
-  ctx.lineWidth = 8; // ancho linea
-let colores = ['#FF5733','#33FF57','#3357FF','#F1C40F','#9B59B6','#E67E22','#1ABC9C','#E74C3C','#34495E','#95A5A6']; 
-  ctx.fillStyle=colores[XX];
-  XX++;
-  if (XX >= colores.length) {
-    XX = 0;
-  }
-  ctx.fillRect(230, 160, 50, 50); //rectangulo luz
-
-  ctx.strokeRect(230, 160, 50, 50); //rectangulo ventana
-  ctx.strokeRect(230, 160, 25, 25); //rectangulo ventana marco
-  ctx.strokeRect(230, 185, 25, 25); //rectangulo ventana marco
-  ctx.strokeRect(255, 160, 25, 25); //rectangulo ventana marco
-  ctx.fillStyle='brown';
-  ctx.strokeRect(130, 190, 40, 60); //rectangulo puerta
-  ctx.fillRect(130, 190, 40, 60); //rectangulo puerta
-  ctx.strokeRect(75, 140, 250, 110); //rectangulo casa
-
-  ctx.beginPath(); //poligono techo
-  ctx.moveTo(50, 140);
-  ctx.lineTo(200, 60);
-  ctx.lineTo(350, 140);
-  ctx.closePath();
-  ctx.stroke();  //fin poligono
-
-  ctx.fillStyle='red';
-
-  ctx.font = "30px serif"; //texto
-  ctx.fillText("Casita", 90, 170)
-  canvas.id = "miCanvas";
-}
-
-
-/*const canvasSketch = require('canvas-sketch');
+const canvasSketch = require('canvas-sketch'); //importamos
 
 const settings = {
-  dimensions: [1080, 1080],
+  dimensions: [1080, 1080], //tamaño de canvas
+  animate: true, //se llama la funcion renderizado (abajo) 60 veces por segundo
 };
 
-const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'blue';
-    context.fillRect(0, 0, width, height);
+const sketch = ({ context, width, height }) => {
+  let XX=0,YY=0;
+  context.fillStyle = '#B2E4EB';
+  context.fillRect(0, 0, width, height);  //fill relleno
+  //si pones esots solo limpi 1 vez
+  return ({ context, width, height }) => {// funcion renderizado
+  //  context.fillStyle = '#B2E4EB';
+  //  context.fillRect(0, 0, width, height);  //fill relleno
+//no limplia
+
+    ///////////////
+    context.fillStyle = '#52342E';
+    // Set line width
+      context.lineWidth = 10;
+
+      // Wall
+      context.strokeRect(XX+75, YY+140, 150, 110);
+
+      // Door
+      context.fillRect(XX+130, YY+190, 40, 60);
+      XX+=5;
+      // Roof
+      context.beginPath();
+      context.moveTo(XX+50, YY+140);
+      context.lineTo(XX+150, YY+60);
+      context.lineTo(XX+250, YY+140);
+      context.closePath();
+      context.stroke();
+
+if (XX>=800){
+  XX=0;
+  YY+=100;
+}
+
+
+    /////////////
   };
 };
 
 canvasSketch(sketch, settings);
-*/
+
 
 
 
